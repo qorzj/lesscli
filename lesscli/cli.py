f2db770b09f4d6331a6da5f881d2ac08c9bc2381
qorzj <zhangji@parsec.com.cn>
2020-01-09 21:18:46 +0800

    1. new feature: Sub Application; 2. 0.0.8 → 0.0.9

--- lesscli/cli.py
+++ lesscli/cli.py
@@ -1,56 +1,67 @@
 import sys
 
 
-def is_sub_path(sub_path, path):  # type: (str, str) -> bool
-    path_list = path.split('/') if path else []
-    sub_path_list = sub_path.split('/') if sub_path else []
-    return sub_path_list[:len(path_list)] == path_list and len(path_list) + 1 == len(sub_path_list)
-
-
 def doc_text(dealer):  # (...) -> str
     if not dealer.__doc__:
         return ''
     return dealer.__doc__
 
 
-def show_help(dealer, sub_help_doc):
+def show_help(dealer):
     print(doc_text(dealer))
-    print()
-    if sub_help_doc:
-        print(sub_help_doc)
     print()
 
 
 class Application:
-    def __init__(self, doc):
+    def __init__(self, doc=''):
         self.doc = doc
         self.mapping = []
 
-    # 获取最长匹配到path的dealer
-    def _match_dealer(self, arg_list):  # type: ('Application', list) -> tuple
-        arg_path = '/'.join(arg_list)
-        for path, dealer in sorted(self.mapping, key=lambda x: x[0], reverse=True):
-            if arg_path == path or arg_path.startswith(path + '/'):
-                return path, dealer, path.count('/') + 1
-        return None, None, None
+    # 获取最长匹配到path的dealer函数
+    # arg_list是不含减号的argv前缀部分
+    # 返回path, dealer, level
+    def _match_dealer(self, arg_list):
+        cur_app = self
+        cur_level = 0
+        cur_path = ()
+        if not arg_list:
+            return cur_path, cur_app, cur_level
+        while True:
+            quit = True
+            for path, dealer in cur_app.mapping:
+                if arg_list[cur_level] == path:
+                    cur_level += 1
+                    cur_path += (path,)
+                    cur_app = dealer
+                    if isinstance(dealer, Application):
+                        quit = cur_level == len(arg_list)
+                    break
+            if quit:
+                return cur_path, cur_app, cur_level
+
 
     # 获取path的下一级列表即帮助文本
-    def _sub_help_doc(self, path):  # type: ('Application', str) -> str
+    def _sub_help_doc(self, path, app):  # type: (...) -> str
         cmd = sys.argv[0]
         ret = ''
-        if not path:
-            ret += self.doc + '\n'
-        for sub_path, dealer in self.mapping:
-            if is_sub_path(sub_path, path):
-                ret += '  ' + cmd + ' ' + sub_path.replace('/', ' ') + '\t\t' \
-                       + doc_text(dealer).strip().splitlines()[0].strip() + '\n'
+        if app is None:
+            app = self
+        if not isinstance(app, Application):
+            raise ValueError('app must be Application')
+        ret += app.doc + '\n'
+        for sub_path, dealer in app.mapping:
+            ret += '  ' + cmd + ' ' + ' '.join(list(path)) + ' ' + sub_path + '\t\t'
+            if isinstance(dealer, Application):
+                ret += dealer.doc.strip().splitlines()[0].strip() + '\n'
+            else:
+                ret += doc_text(dealer).strip().splitlines()[0].strip() + '\n'
         return ret
 
     def add(self,
             path,  # type: str
-            dealer):
+            dealer_or_app):
         # type: (...) -> 'Application'
-        self.mapping.append((path.strip('/'), dealer))
+        self.mapping.append((path.strip('/'), dealer_or_app))
         return self
 
     def run(self):  # type: ('Application') -> None
@@ -81,21 +92,22 @@ class Application:
                 list_params.append(arg)
             i += 1
         path, dealer, level = self._match_dealer(arg_list)   # 最长匹配
-        if dealer is None:
-            print(self._sub_help_doc(''))
+        if dealer is None or isinstance(dealer, Application):
+            print(self._sub_help_doc(path, dealer))
             print()
-        elif 'h' in dict_params or 'help' in dict_params:
-            show_help(dealer, self._sub_help_doc(path))
+            return
+        if 'h' in dict_params or 'help' in dict_params:
+            show_help(dealer)
         else:
             try:
                 dealer(*list_params[level:], **dict_params)
             except TypeError as e:
                 if str(e).startswith(dealer.__name__ + '() '):
-                    show_help(dealer, self._sub_help_doc(path))
+                    show_help(dealer)
                 else:
                     raise
             except AssertionError:
-                show_help(dealer, self._sub_help_doc(path))
+                show_help(dealer)
 
 
 def run(callback):
